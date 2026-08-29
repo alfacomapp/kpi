307afaef6108f3288571c2945ceb6288f900c22d
alfacomapp <alfacomputerofficial@gmail.com>
2026-08-29 11:39:58 +0800

    Create config.js

---
+++ docs/assets/js/config.js
@@ -0,0 +1,10 @@
+(function () {
+  window.KPI_CONFIG = {
+    APP_NAME: "KPI Karyawan",
+    API_BASE_URL: "https://kpi.alfacomapp.workers.dev",
+    DEMO_MODE_WHEN_API_EMPTY: true,
+    MAX_UPLOAD_MB: 8,
+    SESSION_STORAGE_KEY: "kpi_session_v1",
+    BIOMETRIC_STORAGE_KEY: "kpi_biometric_v1"
+  };
+})();
